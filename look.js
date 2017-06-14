9a9c54b0fd73de39f61a7c114d48d27023b3f8b4
liumin1128 <970568830@qq.com>
2017-06-14 08:17:49 +0800

    更新

---
+++ look.js
@@ -0,0 +1,16 @@
+﻿/*
+* @Author: 刘敏
+* @Date:   2016-02-17 20:53:06
+* @Last Modified by:   刘敏
+* @Last Modified time: 2016-02-19 09:05:19
+*/
+const proxy = require('express-http-proxy');
+const express = require('express');
+
+const app = express();
+const server = require('http').createServer(app);
+
+app.use('/api', proxy('liumin.me:3000'));
+app.use('/', express.static(`${__dirname}/dist`));
+server.listen(8001);
+console.log('服务器8001');
